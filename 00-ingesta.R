99c7946883b8209aaec97a40de49c0b0c6c141be
thiagomottaa <mottasantigo@gmail.com>
2026-04-11 10:21:47 -0300

    Cambios al archivo INGESTA.

--- 00-ingesta.R
+++ 00-ingesta.R
@@ -1,16 +1,35 @@
-# Instalo los paquetes necesarios (si aún no los tengo instalados)
-# install.packages("googlesheets4")
-
 library(googlesheets4)
 
 # Link al archivo
-url="https://docs.google.com/spreadsheets/d/1VSNMETw4OjxadPc60S6oAbC13al2FwAx1_LSIciShag/edit?usp=sharing"
+url="https://docs.google.com/spreadsheets/d/1Kwl4KByOv8q2kXMsgaO3d5QI3vUQ40RCZJgJHhg5bmE/edit?pli=1&gid=1481484702#gid=1481484702"
 
 # Evito loggeo
 gs4_deauth()
 
 # Leo el archivo y almaceno los datos en un data frame
-datos <- read_sheet(url, skip = 2)
+datos <- read_sheet(url, skip = 1,sheet=2)
 
-# Veo la estructura del dataset
+# Definimos el orden lógico de menor a mayor
+niveles_orden <- c("Muy bajo", "Bajo", "Medio", "Alto", "Muy alto")
+names(datos)[18] <- "dim_mejor_punt"
+
+
+# Transformamos las columnas de texto a categorías (Factores)
+datos$GIRAI_region <- as.factor(datos$GIRAI_region)
+datos$NU_region <- as.factor(datos$NU_region)
+datos$UN_subregion <- as.factor(datos$UN_subregion)
+datos$sec_mng <- factor(datos$sec_mng, levels = niveles_orden, ordered = TRUE)
+datos$sec_ag  <- factor(datos$sec_ag,  levels = niveles_orden, ordered = TRUE)
+datos$sec_ane <- factor(datos$sec_ane, levels = niveles_orden, ordered = TRUE)
+datos$GIRAI_region   <- as.factor(datos$GIRAI_region)
+datos$NU_region      <- as.factor(datos$NU_region)
+datos$UN_subregion   <- as.factor(datos$UN_subregion)
+datos$academia       <- as.factor(datos$academia)
+datos$privado        <- as.factor(datos$privado)
+datos$dim_mejor_punt <- as.factor(datos$dim_mejor_punt)
+datos$tipo_academia_es <- as.factor(datos$tipo_academia_es)
+datos$tipo_privado_es  <- as.factor(datos$tipo_privado_es)
+# Ahora probá correr el summary de nuevo
+summary(datos)
+
 str(datos)
